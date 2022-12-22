6e90630b690332134a3bfd9972b8bbca46485e83
bhandekunal16 <bhandekunal16@gmail.com>
2022-12-22 11:40:51 +0530

    update

--- first page.js
+++ first page.js
@@ -6,15 +6,6 @@ function calculatecircumfaran() {
     return circumfarance.innerHTML = (2 * 3.14 * `${radius.value}`);
 }
 
-// hello full namewith age
-let firstn= document.getElementById ("firstn");
-let lastn= document.getElementById ("lastn");
-let age= document.getElementById ("age");
-let name_age=document.getElementById("name_age");
-console.log (name_age);
-function calculatename() {
-    return name_age.innerHTML = ( ` hello ${firstn.value}` + "  " + `${lastn.value}` + " your age is " + `${age.value}`);
-}
 
 // even-odd number identifier
 let num= document.getElementById ("num");
@@ -30,24 +21,6 @@ function getEvenOdd() {
 
     }
 }
-
-// age calculater
-let presentyear= document.getElementById ("presentyear");
-let birthyear= document.getElementById ("birthyear");
-let wishme= document.getElementById ("wishme");
-console.log (wishme);
-function getmybirthday() {
-    return wishme.innerHTML=(`${presentyear.value}`-`${birthyear.value}`+" "+ "year");
-}
-
-// BMI calculater
-let height= document.getElementById ("height");
-let weight= document.getElementById ("weight");
-let BMI= document.getElementById ("BMI");
-console.log (BMI);
-function getmyBMI (){
-    return BMI.innerHTML= (weight.value / ((height.value * height.value)/10000)).toFixed(2)
-}
  
 // percentage calculaterS
 let value= document.getElementById ("value");
@@ -56,17 +29,6 @@ let percentage= document.getElementById ("percentage");
 console.log (percentage);
 function getmypercentage () {
     return percentage.innerHTML=((value.value/totalvalue.value)*100 + "%")
-}
-
-// BMR calculater
-let Height= document.getElementById ("Height");
-let Weight= document.getElementById ("Weight");
-let Age= document.getElementById ("Age") ;
-let BMR= document.getElementById ("BMR");
-console.log (BMR); 
-function calculateBMR()
-{ 
-    return BMR.innerHTML= (((13.397* Weight.value)+(4.799 * Height.value) - (5.677 * Age.value)) + 88.362);
 }
 
 
