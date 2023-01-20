b0921617dc504817ca58c34798dfbd217718efb1
smart school <you@example.com>
2023-01-20 23:23:14 +0530

    update

---
+++ otp genrater.js
@@ -0,0 +1,5 @@
+let value=document.getElementById("value")
+let screen=document.getElementById("screen")
+function getotp() {
+    return screen.innerHTML = Math.floor((Math.random() * `${value.value}`) + 1);
+}
